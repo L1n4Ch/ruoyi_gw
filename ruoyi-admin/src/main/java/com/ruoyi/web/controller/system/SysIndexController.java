9a8daa65e4e6a345f6cc6c9f47b3b95f0ca03d66
L1n4Ch <lin337982849@gmail.com>
2023-03-27 08:24:04 +0800

    修改后台管理系统的首页

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysIndexController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysIndexController.java
@@ -131,7 +131,7 @@ public class SysIndexController extends BaseController
     public String main(ModelMap mmap)
     {
         mmap.put("version", RuoYiConfig.getVersion());
-        return "main";
+        return "cmjlkj/task/task";
     }
 
     // content-main class
